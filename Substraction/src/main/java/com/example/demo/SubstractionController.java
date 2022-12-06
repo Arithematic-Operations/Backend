2ed8d6ff601455cb401010e478744182cb015ac4
linkcode100 <linkcode07@gmail.com>
2022-12-06 19:06:00 +0530

    connection

--- Substraction/src/main/java/com/example/demo/SubstractionController.java
+++ Substraction/src/main/java/com/example/demo/SubstractionController.java
@@ -1,10 +1,11 @@
-package com.example.demo.controller;
+package com.example.demo;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/operation/")
 public class SubstractionController {
